d356a624b9aab2dc3bad0812635d7149e86ed52a
deepakps <pradhan.deepak11@gmail.com>
2020-12-28 02:52:28 +0100

    **refactor**: Structural Changes

--- src/main/java/com/calculator/application/algorithm/Evaluator.java
+++ src/main/java/com/calculator/application/algorithm/Evaluator.java
@@ -3,89 +3,51 @@ package com.calculator.application.algorithm;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-
-import com.calculator.application.model.*;
+import java.util.stream.Collectors;
 
 public class Evaluator {
-	
-	public List<Result> evaluate(List<City> cities, List<Operation> operations) throws Exception {		
+
+	public List<Result> evaluate(List<City> cities, List<Operation> operations) {
 		final String ATTRIB = "attrib";
 		final String SUB = "sub";
-		
+
 		final String POPULATION = "population";
 		final String AREA = "area";
-		
+
 		List<Result> results = new ArrayList<Result>();
-		
-		List<Double> listData = null;
-		
-		for(Operation operation: operations) {
-			Result result = new Result();
-			listData = new ArrayList<Double>();
-			if(operation.getType().equals(ATTRIB)) {
-				if(operation.getAttrib().equals(POPULATION)) {
-					for(City city: cities) {
-						if(city.getName().matches(operation.getFilter()))
-						{
-							listData.add(city.getPopulation());
-						}
-					}
+
+		List<Result> populationResults = operations.stream().filter(operation -> operation.getType().equals(ATTRIB))
+				.filter(operation -> operation.getAttrib().equals(POPULATION)).map(operation -> {
+					Result result = new Result();
 					result.setName(operation.getName());
-					result.setValue(operate(listData, operation.getFunc()));	
-					results.add(result);
-				}
-			}
-			else if(operation.getType().equals(SUB)) {
-				if(operation.getAttrib().equals(AREA)) {
-					for(City city: cities) {
-						if(city.getName().matches(operation.getFilter()))
-						{
-							listData.add(city.getArea());
-						}
-					}
+					result.setValue(operate(
+							cities.stream().filter(city -> city.getName().matches(operation.getFilter()))
+									.map(city -> city.getPopulation()).collect(Collectors.toList()),
+							operation.getFunc()));
+					return result;
+				}).collect(Collectors.toList());
+		results.addAll(populationResults);
+
+		List<Result> areaResults = operations.stream().filter(operation -> operation.getType().equals(SUB))
+				.filter(operation -> operation.getAttrib().equals(AREA)).map(operation -> {
+					Result result = new Result();
 					result.setName(operation.getName());
-					result.setValue(operate(listData, operation.getFunc()));	
-					results.add(result);
-				}
-			}
-		}
+					result.setValue(
+							operate(cities.stream().filter(city -> city.getName().matches(operation.getFilter()))
+									.map(city -> city.getArea()).collect(Collectors.toList()), operation.getFunc()));
+					return result;
+				}).collect(Collectors.toList());
+		results.addAll(areaResults);
+
 		return results;
 	}
-	
 
-	
-	private static String operate(List<Double> list, String funcType) throws Exception
-	{
+	private static String operate(List<Double> list, String funcType) {
 		final DecimalFormat decimalFormat = new DecimalFormat("#.00");
 		decimalFormat.setRoundingMode(RoundingMode.HALF_DOWN);
-		
-		final String AVERAGE = "average";
-		final String MAX = "max";
-		final String MIN = "min";
-		final String SUM = "sum";
-		
-		if(funcType.equals(AVERAGE)) {
-			double sum = toDouble(list);
-			return decimalFormat.format(sum/ list.size());
-		}
-		else if(funcType.equals(MAX)) {
-			Double max = Collections.max(list);
-			return decimalFormat.format(max);
-		}
-		else if(funcType.equals(MIN)) {
-			Double max = Collections.min(list);
-			return decimalFormat.format(max);
-		}
-		else if(funcType.equals(SUM)) {
-			double sum = toDouble(list);
-			return decimalFormat.format(sum);
-		}
-		return "0.00";
+
+		return decimalFormat.format(OperationFactory.valueOf(funcType.toUpperCase()).apply(list));
 	}
-	
-	private static double toDouble(List<Double> list) {
-		return list.stream().mapToDouble(Double::doubleValue).sum();
-	}
+
 }
